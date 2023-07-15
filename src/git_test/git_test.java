e668ff9473a472e865ddc7cbdfb54791afed7bc7
jueongim <jueongim202@gmail.com>
2023-07-15 20:20:11 +0900

    second test

--- src/git_test/git_test.java
+++ src/git_test/git_test.java
@@ -2,6 +2,17 @@ package git_test;
 
 public class git_test {
 
+	public class git_test2 {
+	
+		public static void main(String[] args) {
+			System.out.println("test2 ");
+			//github test
+			// TODO Auto-generated method stub
+	
+		}
+	
+	}
+
 	public static void main(String[] args) {
 		System.out.println("test");
 		//github test
